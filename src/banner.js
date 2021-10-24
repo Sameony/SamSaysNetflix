c998d8d8296719d9629b2420e92bd1483f2e25cd
Sameony <iam2001sam@gmail.com>
2021-10-24 19:59:50 +0530

    feat: successful api call

--- src/banner.js
+++ src/banner.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './banner.css';
+import axios from './axios';
+import requests from './request';
 
-function banner() {
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData()
+        {
+            const request = await axios.get(requests.fetchActionMovie);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length-1)
+            ]
+            );
+            return request;
+        }
+    fetchData();
+}, []);
+
+
+console.log(movie);
+
+
 function truncate(string, n)
 {
     return string?.length>n?string.substring(0, n-1)+'...':string;
@@ -28,4 +51,4 @@ function truncate(string, n)
     )
 }
 
-export default banner
+export default Banner
